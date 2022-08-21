ab1563a3700f73355b785f04e532c37623f791bb
Kelliton Santana De Souza <programmerKelliton@gmail.com>
2022-08-21 10:53:21 -0300

    Adicionando cores aos cards

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -9,7 +9,21 @@ function App() {
     return (
         <div className="App">
             <div className="App__cards">
-                <Card name=".." desc="..." icon={IconCard1}></Card>
+                <Card
+                    name="Sedans"
+                    desc="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
+                    icon={IconCard1}
+                ></Card>
+                <Card
+                    name="SUVs"
+                    desc="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
+                    icon={IconCard2}
+                ></Card>
+                <Card
+                    name="Luxury"
+                    desc="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
+                    icon={IconCard3}
+                ></Card>
             </div>
         </div>
     );
